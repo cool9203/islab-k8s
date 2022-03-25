ab6085bfd45b5034e05d38be5bb0f0eab07f68cf
cool9203 <octer18@gmail.com>
2022-03-25 17:05:18 +0800

    fixed: register-root.py always show False. and web/register.html memory range max is 4096, now unlimit.

--- backend/script/register-root.py
+++ backend/script/register-root.py
@@ -4,6 +4,5 @@ sys.path.insert(0, str(Path("../").resolve()))
 sys.path.insert(0, str(Path("./").resolve()))
 
 import pkg.dbapi
-pkg.dbapi.register("islab", "islabs102a", "islab", 0)
 print("register-root result : ", pkg.dbapi.register("islab", "islabs102a", "islab", 0))
 
